3f511952b9c0b7ad9ad5c4b5974b60f1aba1e1da
eeesnghyun <szhyun2002@gmail.com>
2024-04-28 17:39:35 +0900

    [feature] 웹 뷰 위젯 추가

--- lib/widget/webview.dart
+++ lib/widget/webview.dart
@@ -1,6 +1,3 @@
-import 'dart:convert';
-import 'dart:io';
-import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:webview_flutter/webview_flutter.dart';
@@ -15,33 +12,24 @@ class MainWebView extends StatefulWidget {
 }
 
 class _MainWebViewState extends State<MainWebView> {
-  final String _webviewChannel =
-      Platform.isAndroid ? 'androidChannel' : 'iosChannel';
   late WebViewController _controller = WebViewController();
-  bool _isLoading = true;
 
   @override
   void initState() {
-    Future.delayed(const Duration(seconds: 2), () {
-      _controller = WebViewController()
-        ..setJavaScriptMode(JavaScriptMode.unrestricted)
-        ..setNavigationDelegate(
-          NavigationDelegate(
-            onProgress: (int progress) {},
-            onPageStarted: (String url) {},
-            onPageFinished: (String url) async {
-              setState(() {
-                _isLoading = false;
-              });
-            },
-            onWebResourceError: (WebResourceError error) {},
-            onNavigationRequest: (NavigationRequest request) {
-              return NavigationDecision.navigate;
-            },
-          ),
-        )
-        ..loadRequest(Uri.parse('${dotenv.env['BASE_URL']}'));
-    });
+    _controller = WebViewController()
+      ..setJavaScriptMode(JavaScriptMode.unrestricted)
+      ..setNavigationDelegate(
+        NavigationDelegate(
+          onProgress: (int progress) {},
+          onPageStarted: (String url) {},
+          onPageFinished: (String url) async {},
+          onWebResourceError: (WebResourceError error) {},
+          onNavigationRequest: (NavigationRequest request) {
+            return NavigationDecision.navigate;
+          },
+        ),
+      )
+      ..loadRequest(Uri.parse('${dotenv.env['BASE_URL']}'));
 
     super.initState();
   }
