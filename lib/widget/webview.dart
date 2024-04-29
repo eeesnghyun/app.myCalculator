963944929d501daf26152c4b7a8b5ac5c4bd889e
eeesnghyun <szhyun2002@gmail.com>
2024-04-29 23:18:32 +0900

    [feature] 카카오 공유하기 추가

--- lib/widget/webview.dart
+++ lib/widget/webview.dart
@@ -2,6 +2,8 @@ import 'dart:convert';
 
 import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:fluttertoast/fluttertoast.dart';
+import 'package:kakao_flutter_sdk_share/kakao_flutter_sdk_share.dart';
 import 'package:webview_flutter/webview_flutter.dart';
 
 class MainWebView extends StatefulWidget {
@@ -15,22 +17,108 @@ class MainWebView extends StatefulWidget {
 
 class _MainWebViewState extends State<MainWebView> {
   late WebViewController _controller = WebViewController();
+  late FToast fToast;
+
+  void _openToast(
+    String message,
+    ToastGravity gravity,
+  ) {
+    Widget toast = Container(
+      padding: const EdgeInsets.symmetric(horizontal: 24.0, vertical: 12.0),
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(25.0),
+        color: const Color(0XFF0479f6),
+      ),
+      child: Row(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          const Icon(Icons.notifications_active, color: Colors.white),
+          const SizedBox(width: 12.0),
+          Text(
+            message,
+            style: const TextStyle(color: Colors.white),
+          ),
+        ],
+      ),
+    );
+
+    fToast.showToast(
+      child: toast,
+      gravity: gravity,
+      toastDuration: const Duration(seconds: 2),
+    );
+  }
 
   @override
   void initState() {
+    fToast = FToast();
+    fToast.init(context);
+
     _controller = WebViewController()
       ..setJavaScriptMode(JavaScriptMode.unrestricted)
       ..addJavaScriptChannel('shareChannel',
           onMessageReceived: (JavaScriptMessage javaScriptMessage) async {
         var data = jsonDecode(javaScriptMessage.message);
+        String paymentMethod = data['paymentMethod'];
+        String amount = data['amount'];
+        String period = data['period'];
+        String interest = data['interest'];
+        String redirectUrl =
+            '${dotenv.env['BASE_URL']}/share?paymentMethod=$paymentMethod&amount=$amount&period=$period&interest=$interest';
 
-        print(data);
+        try {
+          bool isKakaoTalkSharingAvailable =
+              await ShareClient.instance.isKakaoTalkSharingAvailable();
+          if (isKakaoTalkSharingAvailable) {
+            String methodName = "";
+
+            switch (paymentMethod) {
+              case "1":
+                methodName = "원리금 균등 상환";
+                break;
+              case "2":
+                methodName = "원금 균등 상환";
+                break;
+              case "3":
+                methodName = "만기일시 상환";
+                break;
+            }
+
+            FeedTemplate defaultFeed = FeedTemplate(
+              content: Content(
+                title: '$amount원을 $methodName으로 $period년 대출하면..',
+                description: '연 이율이 $interest%일 때, 매월 얼마를 상환해야 할까요?',
+                imageUrl: Uri.parse('${dotenv.env['BASE_URL']}/favicon.png'),
+                link: Link(
+                    webUrl: Uri.parse(redirectUrl),
+                    mobileWebUrl: Uri.parse(redirectUrl)),
+              ),
+              buttons: [
+                Button(
+                  title: '계산 결과 확인하기',
+                  link: Link(
+                    webUrl: Uri.parse(redirectUrl),
+                    mobileWebUrl: Uri.parse(redirectUrl),
+                  ),
+                ),
+              ],
+            );
+
+            Uri uri =
+                await ShareClient.instance.shareDefault(template: defaultFeed);
+            await ShareClient.instance.launchKakaoTalk(uri);
+          } else {
+            _openToast("카카오톡을 설치해 주세요.", ToastGravity.BOTTOM);
+          }
+        } catch (error) {
+          _openToast("공유하기에 실패했습니다.", ToastGravity.TOP);
+        }
       })
       ..addJavaScriptChannel('alertChannel',
           onMessageReceived: (JavaScriptMessage javaScriptMessage) {
         var data = jsonDecode(javaScriptMessage.message);
 
-        print(data);
+        _openToast(data["message"], ToastGravity.TOP);
       })
       ..setNavigationDelegate(
         NavigationDelegate(
